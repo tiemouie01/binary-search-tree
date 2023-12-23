7f0c7efecb8d6bd98361d4a5476a58aca305165c
Timothy Miamba <timal0361@gmail.com>
2023-12-23 11:12:39 +0200

    Add prettyPrint() which logs the tree in a better visual format

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 import Tree from "./tree.js";
 
-const test = Tree();
 
-console.log(test.buildTree([1, 2, 3, 4, 5, 6, 7]));
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const test = Tree([1, 2, 3, 4, 5, 6, 7]);
+
+prettyPrint(test.root);
--- tree.js
+++ tree.js
@@ -1,32 +1,34 @@
 const Node = function binaryTreeNode(data, left = null, right = null) {
-    return {
-        data,
-        left,
-        right,
-    };
-}
-
-export default function Tree() {
-  const buildTree = function buildTheBinarySearchTree(array) {
-    if (array.length === 1) {
-      return array[0];
-    }
-
-    // Set the middle element of the array as root.
-    const middle = Math.floor(array.length / 2);
-    const root = Node(array[middle]);
-
-    /** Recursively do the same for the left and right
-     * Set the root of the left half of the array and make it the left child of the main root
-     * Repeat for the right half of the array.
-     */
-    root.left = buildTree(array.slice(0,middle));
-    root.right = buildTree(array.slice(middle + 1));
-    
-    return root;
+  return {
+    data,
+    left,
+    right,
   };
+};
+
+const buildTree = function buildTheBinarySearchTree(array) {
+  if (!array.length) {
+    return null;
+  }
+
+  // Set the middle element of the array as root.
+  const middle = Math.floor(array.length / 2);
+  const root = Node(array[middle]);
+
+  /** Recursively do the same for the left and right
+   * Set the root of the left half of the array and make it the left child of the main root
+   * Repeat for the right half of the array.
+   */
+  root.left = buildTree(array.slice(0, middle));
+  root.right = buildTree(array.slice(middle + 1));
+
+  return root;
+};
+
+export default function Tree(array) {
+  const root = buildTree(array);
 
   return {
-    buildTree,
+    root,
   };
 }
