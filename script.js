e5095d39638e00a9adf533b360bcfb24c30e8658
Timothy Miamba <timal0361@gmail.com>
2023-12-29 10:14:04 +0200

    Add isBalanced() method

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ const test = Tree([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12]);
 test.insert(15);
 test.insert(20);
 test.insert(13);
-test.insert(17);
+// test.insert(17);
 
 prettyPrint(test.root);
 
 const node = test.find(6);
 console.log(`The height of the tree is ${test.height(node)}`);
-// console.log(test.isBalanced());
+console.log(test.isBalanced());
